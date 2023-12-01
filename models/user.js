d45a200c662c9980984f5807adb3b57b90faa121
Nel <nel.renaudin@gmail.com>
2023-12-01 15:30:02 +0100

    Add password field and set email field unique to true in user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   bio: { type: String, required: false, length: { min: 2 } },
 })
 
