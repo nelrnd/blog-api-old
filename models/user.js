0d470618a58cfc03b5e7b28a769eb11190205858
Nel <nel.renaudin@gmail.com>
2023-12-02 15:12:44 +0100

    Add pre-save and isValidPassword user model methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const bcrypt = require("bcryptjs")
 
 const userSchema = new Schema({
   username: { type: String, required: true },
@@ -11,5 +12,16 @@ const userSchema = new Schema({
 userSchema.virtual("url").get(function () {
   return `/users/${this._id}`
 })
+
+userSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10)
+  this.password = hash
+  next()
+})
+
+userSchema.methods.isValidPassword = async function (password) {
+  const match = await bcrypt.compare(password, this.password)
+  return match
+}
 
 module.exports = mongoose.model("User", userSchema)
